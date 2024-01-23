ed4240df7fcf610b38bd8f067bb22c19746a3692
wada-mitsuki <wada.mitsuki@suzukishouten.co.jp>
2024-01-23 11:43:09 +0900

    husky確認-コミットできたら成功

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,6 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
 
-// ESlint確認用
-type WadamituskiPrps = {};
-
 export default function Document() {
   return (
     <Html lang="en">
